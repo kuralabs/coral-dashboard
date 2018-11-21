5042e0aa046097a7c045bbf9ab41af83bfa47c4e
Carolina Aguilar <carolina.aguilar@kuralabs.io>
2018-11-21 03:17:17 -0600

    new: dev: Widgets layout prototype.

--- dashboard/coral_dashboard/ui/coral.py
+++ dashboard/coral_dashboard/ui/coral.py
@@ -22,9 +22,12 @@ Coral UI module.
 from logging import getLogger as get_logger
 
 from urwid import (
+    Pile,
     Text,
     Filler,
     AttrMap,
+    Columns,
+    Divider,
 )
 
 
@@ -36,17 +39,47 @@ class CoralUI:
     FIXME: Document.
     """
     palette = [
-        ('style1', 'black', 'light gray'),
-        ('style2', 'black', 'dark red'),
-        ('style3', 'black', 'dark blue'),
+        ('widget_header',   'white,bold',   'dark cyan',),
+        ('section_header',  'white',        'dark magenta',),
+        ('style1',          'black',        'light gray'),
+        ('style2',          'white',        'black'),
     ]
 
     def __init__(self):
-        self.header = Text(('style1', u" Hello World "), align='center')
-        map1 = AttrMap(self.header, 'style2')
-        fill = Filler(map1)
-        self.topmost = AttrMap(fill, 'style3')
+        self.widgets_config = [
+            { 'title': 'Temperature', 'sections': ['Coolant', 'GPU', 'CPU'] },
+            { 'title': 'Load', 'sections': ['GPU', 'CPU']}
+        ]
 
+        self.header = Text(('style1', u' Hello World '), align='center')
+        map1 = AttrMap(self.header, 'style1')
+
+        self.widgets = Pile([self.get_widget(w) for w in self.widgets_config])
+        fill = Filler(Pile([map1, self.widgets]), 'top')
+        self.topmost = AttrMap(fill, 'style2')
+
+    def get_widget(self, w):
+        header = Text(('widget_header', w['title']))
+        header_map = AttrMap(header, 'widget_header')
+
+        div = Divider(u'=')
+        div_map = AttrMap(div, 'widget_header')
+        space = Divider(u' ')
+
+        sections = [self.get_section(s) for s in w['sections']]
+        cols = Columns(sections, dividechars=1)
+
+        pile = Pile([header_map, div_map, cols, space])
+
+        return pile
+
+    def get_section(self, title):
+        header = Text(('section_header', title), align='center')
+        header_map = AttrMap(header, 'section_header')
+
+        pile = Pile([header_map])
+
+        return pile
 
 __all__ = [
     'CoralUI',
